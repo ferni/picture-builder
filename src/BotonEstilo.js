469b17be79693961876022697045d1bf2ebeab3c
Fernando Tiberti <fertiberti@gmail.com>
2016-08-22 01:21:09 -0300

    Tweak botonEstilo

--- src/BotonEstilo.js
+++ src/BotonEstilo.js
@@ -4,12 +4,10 @@ import './BotonEstilo.css';
 class BotonEstilo extends Component {
   render() {
     return (
-      <div className="BotonEstilo seleccionable">
-        <div onClick={this.props.onClick}>
+      <div className="BotonEstilo seleccionable"  onClick={this.props.onClick}>
           <img src={this.props.imgSrc} alt={this.props.descripcion}/><br/>
           <span>{this.props.nombre}</span><br/>
           <span>Desde ${this.props.precioMinimo}</span>
-        </div>
       </div>
     );
   }
