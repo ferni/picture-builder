e5c4fdc4ab2b21321b46503d3dabf72c43fba2bf
Fernando Tiberti <fertiberti@gmail.com>
2016-08-28 05:52:17 -0300

    Improved screen 1 appearance

--- src/PanelMaterial.js
+++ src/PanelMaterial.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './PanelMaterial.css';
+import EnhancedButton from 'material-ui/internal/EnhancedButton';
+import Paper from 'material-ui/Paper';
 
 function capitalize(str) {
   var primera = str.substring(0, 1);
@@ -12,11 +14,15 @@ class PanelMaterial extends Component {
   render() {
     var material = capitalize(this.props.material);
     return (
-      <div className="PanelMaterial seleccionable" onClick={this.props.onClick}>
-          <img src={this.props.imgSrc} alt={'Seleccionar ' + material}/>
-          <h4>{material}</h4>
-          <p>{this.props.descripcion}</p>
-      </div>
+      <Paper className="PanelMaterial seleccionable" onTouchTap={this.props.onClick}>
+        <EnhancedButton className="EnhancedButton">
+
+              <img src={this.props.imgSrc} alt={'Seleccionar ' + material}/>
+              <h4>{material}</h4>
+              <p>{this.props.descripcion}</p>
+
+        </EnhancedButton>
+      </Paper>
     );
   }
 }
