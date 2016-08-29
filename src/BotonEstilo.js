8106044c1b7cde2f4ea4b1e69e908a343657813f
Fernando Tiberti <fertiberti@gmail.com>
2016-08-29 02:47:51 -0300

    Tweak boton estilo style

--- src/BotonEstilo.js
+++ src/BotonEstilo.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 import './BotonEstilo.css';
-
+import EnhancedButton from 'material-ui/internal/EnhancedButton.js';
 class BotonEstilo extends Component {
   render() {
     return (
-      <div className="BotonEstilo seleccionable"  onClick={this.props.onClick}>
-          <img src={this.props.imgSrc} alt={this.props.descripcion}/><br/>
-          <h3>{this.props.nombre}</h3>
-          <p>Desde ${this.props.precioMinimo}</p>
+      <div  className="BotonEstilo seleccionable"  onTouchTap={this.props.onClick}>
+        <EnhancedButton className="EnhancedButton">
+
+            <img src={this.props.imgSrc} alt={this.props.descripcion}/><br/>
+            <h3>{this.props.nombre}</h3>
+            <p>Desde ${this.props.precioMinimo}</p>
+        </EnhancedButton>
       </div>
     );
   }
