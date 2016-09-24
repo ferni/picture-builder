d8c90b0d5d3312602cf0b8b443f04211b8baf237
Fernando Tiberti <fertiberti@gmail.com>
2016-09-24 00:44:01 -0300

    Add a proper arrow icon to the dropdown

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import './Dropdown.css';
+import FontIcon from 'material-ui/FontIcon';
+
+var arrowStyle = {
+  fontSize: '40px',
+  marginTop: '10px'
+}
 
 class Dropdown extends Component {
   render() {
@@ -8,7 +14,11 @@ class Dropdown extends Component {
         <div className="contenido">
           {this.props.children}
         </div>
-        <div className="flechita">v</div>
+        <div className="flechita">
+          <FontIcon className="material-icons" color="white" style={arrowStyle}>
+            keyboard_arrow_down
+          </FontIcon>
+        </div>
       </div>
     );
   }
