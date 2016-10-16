4e72018f90e2c33e76fbb0e33fdc473ea2fd23a7
Fernando Tiberti <fertiberti@gmail.com>
2016-10-16 20:08:58 -0300

    Can 'upload' images

--- src/Pantalla3/ImageUploads.js
+++ src/Pantalla3/ImageUploads.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import './ImageUploads.css';
+import Image from 'react-image-file';
+import { ImageUploadField,UploadField} from 'react-image-file';
 
  class ImageUploads extends Component {
    constructor(...args) {
      super(...args)
-     this.handleClick = this.handleClick.bind(this)
-     this.state = {};
+     this.addFiles = this.addFiles.bind(this);
+     this.state = {
+       files: []
+     };
    }
-   handleClick() {
-     var result = prompt('asdf');
-     this.setState({img: result});
+   addFiles(files) {
+     console.log(files);
+     var allFiles = this.state.files;
+     for (var i = 0; i < files.length; i++) {
+       allFiles.push(files[i]);
+     }
+     this.setState({files: allFiles});
+     console.log(this.state.files);
    }
    render() {
+     var images = this.state.files.map(file =>
+       <Image file={file} alt='some text' width={100} height={100} />
+     );
      return (
        <div className="ImageUploads">
-        {this.state.img}
-        <button onClick={this.handleClick}>Choose image</button>
+
+        {images}
+
+        <ImageUploadField
+            label='upload images'
+            imageWidth={50}
+            imageHeight={50}
+            multiple
+            onChange={this.addFiles}
+        />
        </div>
      );
    }
